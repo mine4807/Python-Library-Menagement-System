01d07d52daff1330949f760e842095508ec2331c
mine <32925538+mine4807@users.noreply.github.com>
2024-02-19 14:13:37 +0300

    lms_pyhon_bootcamp.py

--- lms_pyhon_bootcamp.py
+++ lms_pyhon_bootcamp.py
@@ -1,6 +1,6 @@
 class Library:
     def __init__(self):
-        self.file=open("books.txt","a+")
+        self.file=open("books.txt","a+",encoding="utf-8")
         
     def __del__(self):
         print("Destructor")
@@ -21,7 +21,8 @@ class Library:
         author=input("Enter the name of the author:")
         year=input("Enter the first release year:")
         pages=input("Enter the number of pages:")
-        self.file.write("\n"+title+","+author+","+year+","+pages)
+        self.file.write(title+","+author+","+year+","+pages+"\n")
+        print(f"The book named {title} has been added to the file.")
         
         
         
@@ -39,16 +40,30 @@ class Library:
         self.file.truncate(0)
         for line in lst_lines:
                 self.file.write(str(line) + "\n")
-        self.list_book()
+        print(f"The book named {title} has been removed from the file.")
         
        
         
 try:
     lib = Library()
-    lib.add_book()
-    lib.add_book()
-    lib.list_book()
-    lib.remove_book()
+    while True:
+        choice=input("""
+        *** MENU***
+        1) List Books
+        2) Add Book
+        3) Remove Book
+        4) Quit
+                     """)
+        if choice == "1":
+            lib.list_book()
+        elif choice == "2":
+            lib.add_book()
+        elif choice == "3":
+            lib.remove_book() 
+        elif choice ==  "4":
+            print("Exiting the program...")
+            break
+        else:
+            print("Please enter 1, 2, 3 or 4")
 finally:
     del lib
-
